bec93670bb616db6b48ed7ea27b3898aa2ab87d9
maku693 <me@maku693.net>
2022-02-13 12:27:03 +0900

    Separate app package

---
+++ src/app.rs
@@ -0,0 +1,61 @@
+use crate::renderer;
+
+pub struct App {
+    event_loop: winit::event_loop::EventLoop<()>,
+    _window: winit::window::Window,
+    renderer: renderer::Renderer,
+}
+
+impl App {
+    pub async fn new() -> Self {
+        let event_loop = winit::event_loop::EventLoop::new();
+        let window = winit::window::WindowBuilder::new()
+            .with_title("Hello, world!")
+            .with_resizable(false)
+            .with_inner_size(winit::dpi::LogicalSize {
+                width: 1280,
+                height: 720,
+            })
+            .build(&event_loop)
+            .expect("Failed to build window");
+
+        let renderer = renderer::Renderer::new(&window).await;
+        renderer.configure_surface(window.inner_size());
+
+        Self {
+            event_loop,
+            _window: window,
+            renderer,
+        }
+    }
+
+    pub fn run(self) {
+        self.event_loop.run(move |e, _, control_flow| {
+            use winit::{
+                event::{Event, WindowEvent},
+                event_loop::ControlFlow,
+            };
+
+            *control_flow = ControlFlow::Wait;
+
+            match e {
+                Event::WindowEvent {
+                    event: WindowEvent::CloseRequested,
+                    ..
+                } => {
+                    *control_flow = ControlFlow::Exit;
+                }
+                Event::WindowEvent {
+                    event: WindowEvent::Resized(size),
+                    ..
+                } => {
+                    self.renderer.configure_surface(size);
+                }
+                Event::MainEventsCleared => {
+                    self.renderer.render();
+                }
+                _ => (),
+            }
+        })
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,8 @@
+mod app;
 mod renderer;
 
 #[tokio::main]
 async fn main() {
-    let event_loop = winit::event_loop::EventLoop::new();
-    let window = winit::window::WindowBuilder::new()
-        .with_title("Hello, world!")
-        .with_resizable(false)
-        .with_inner_size(winit::dpi::LogicalSize {
-            width: 1280,
-            height: 720,
-        })
-        .build(&event_loop)
-        .expect("Failed to build window");
-
-    let renderer = renderer::Renderer::new(&window).await;
-    renderer.configure_surface(window.inner_size());
-
-    tokio::task::block_in_place(move || {
-        event_loop.run(move |e, _, control_flow| {
-            use winit::{
-                event::{Event, WindowEvent},
-                event_loop::ControlFlow,
-            };
-
-            *control_flow = ControlFlow::Wait;
-
-            match e {
-                Event::WindowEvent {
-                    event: WindowEvent::CloseRequested,
-                    ..
-                } => {
-                    *control_flow = ControlFlow::Exit;
-                }
-                Event::WindowEvent {
-                    event: WindowEvent::Resized(size),
-                    ..
-                } => {
-                    renderer.configure_surface(size);
-                }
-                Event::MainEventsCleared => {
-                    renderer.render();
-                }
-                _ => (),
-            }
-        });
-    })
+    let app = app::App::new().await;
+    app.run();
 }
