77e1a45d599d11284322ba252baa535a47f4bcd5
maku693 <me@maku693.net>
2022-02-24 01:07:30 +0900

    Improve support for moving the window between multiple screens

--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,23 @@ fn main() -> Result<()> {
     env_logger::init();
 
     let event_loop = winit::event_loop::EventLoop::new();
+
+    let monitor_size = event_loop
+        .primary_monitor()
+        .context("Failed to get primary monitor")?
+        .size();
+
     let window = winit::window::WindowBuilder::new()
         .with_title("Hello, world!")
-        .with_inner_size(winit::dpi::LogicalSize {
-            width: 640,
-            height: 360,
+        .with_inner_size(winit::dpi::PhysicalSize {
+            width: monitor_size.width / 2,
+            height: monitor_size.height / 2,
         })
         .build(&event_loop)
         .context("Failed to build window")?;
 
     let renderer = renderer::Renderer::new(&window)?;
+
     renderer.configure_surface(window.inner_size());
 
     event_loop.run(move |e, _, control_flow| {
@@ -29,7 +36,12 @@ fn main() -> Result<()> {
         match e {
             Event::WindowEvent { event, .. } => match event {
                 WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
-                WindowEvent::Resized(size) => renderer.configure_surface(size),
+                WindowEvent::Resized(size) => {
+                    renderer.configure_surface(size);
+                }
+                WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
+                    renderer.configure_surface(*new_inner_size);
+                }
                 _ => (),
             },
             Event::MainEventsCleared => {
