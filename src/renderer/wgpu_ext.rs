2194279ae1a74250fff3e3cf216ae7aabe657f7e
maku693 <me@maku693.net>
2022-07-23 16:13:42 +0900

    wip

--- src/renderer/wgpu_ext.rs
+++ src/renderer/wgpu_ext.rs
@@ -11,38 +11,47 @@ pub struct Texture {
 }
 
 impl Texture {
+    #[inline]
     pub fn wgpu_texture(&self) -> &wgpu::Texture {
         &self.wgpu_texture
     }
 
+    #[inline]
     pub fn width(&self) -> u32 {
         self.width
     }
 
+    #[inline]
     pub fn height(&self) -> u32 {
         self.height
     }
 
+    #[inline]
     pub fn depth_or_array_layers(&self) -> u32 {
         self.depth_or_array_layers
     }
 
+    #[inline]
     pub fn mip_level_count(&self) -> u32 {
         self.mip_level_count
     }
 
+    #[inline]
     pub fn sample_count(&self) -> u32 {
         self.sample_count
     }
 
+    #[inline]
     pub fn dimension(&self) -> wgpu::TextureDimension {
         self.dimension
     }
 
+    #[inline]
     pub fn format(&self) -> wgpu::TextureFormat {
         self.format
     }
 
+    #[inline]
     pub fn usage(&self) -> wgpu::TextureUsages {
         self.usage
     }
